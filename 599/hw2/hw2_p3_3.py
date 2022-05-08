aa40ea560cb7bf1115887ea37ed97373a6f99d4c
Corbin Charpentier <corbin.charpentier@gmail.com>
2022-05-07 17:26:37 -0700

    Finish hw2 p3

--- 599/hw2/hw2_p3_3.py
+++ 599/hw2/hw2_p3_3.py
@@ -28,37 +28,36 @@ class Net(nn.Module):
         self.outputLayer = nn.Linear(width, 1)
 
     def forward(self, x):
-        x = torch.flatten(x, 2, 3)
-
+        pp(f"x dims: {x.shape}")
         for l in self.preLayers:
             x = F.relu(l(x))
 
         y = F.relu(self.outputLayer(x))
-        return torch.squeeze(y)
+        return y
 
 
-    def fit(self, batches, loss_fn, optimizer, epochs_n=10): # TODO: change back to 10
-        loss_per_epoch = []
+    def fit(self, xs, ys, learningRate, nEpics=5): # TODO: change back to 10
 
-        for i in range(epochs_n): # TODO no batches
-            batch_loss = 0
-            pp(f"### Epoch #{i}")
+        optimizer = optim.SGD(self.parameters(), lr=learningRate)
+        lossFn = nn.MSELoss()
 
+        lossPerEpoch = []
+
+        for _ in range(nEpics):
             self.train()
-            for image, label in batches:
-                optimizer.zero_grad()
-                net_output = self(image)
-                loss_output = loss_fn(net_output, label)
-                loss_output.backward()
-                optimizer.step()
-                batch_loss += loss_output.item()
 
-            loss_per_epoch.append(batch_loss)
-            pp(f"   loss for this epoch: {loss_per_epoch[i]}" )
+            optimizer.zero_grad()
+            y_hat = self(xs)
+            pp(f"y_hats : {y_hat}")
+            pp(f"ys : {ys}")
+            loss_output = lossFn(y_hat, ys) 
+            loss_output.backward()
+            optimizer.step()
+            lossPerEpoch.append(loss_output.item()/ys.shape[0])
 
-        return loss_per_epoch 
+        return lossPerEpoch 
 
-def genData(n, m):
+def genData(n, m, d):
     def relu(num):
         return 0 if num < 0 else num
     def genNSphere(r, d):
@@ -66,59 +65,72 @@ def genData(n, m):
         d = np.sum(v**2) **(0.5)
         return v/d
 
-    xss = [genNSphere(1, 9) for _ in range(n)]
+    xss = [genNSphere(1, d) for _ in range(n)]
     ys = []
     for xs in xss:
         ys.append(sum(map(relu, xs))/m)
 
     return (np.array(xss), np.array(ys))
 
-############ BEGIN APPLICATION ############ 
-ns = [20, 40, 80, 160]
-d = 10
-m = 5
-
 def calcError(preds, targets):
-    return 0 # TODO
+    pp(f"predictions: {preds}")
+    pp(f"targets: {targets}")
+    return ((preds - targets)**2).mean()
 
 def arcKernel(x1: np.array, x2: np.array):
-    x1Tx2 = np.transpose(x1) * x2
+    x1Tx2 = np.dot(x1, x2)
+    x1Tx2 = 1.0 if x1Tx2 > 1.0 else x1Tx2
+    x1Tx2 = -1.0 if x1Tx2 < -1.0 else x1Tx2
     return x1Tx2*(np.pi - np.arccos(x1Tx2))/(2*np.pi)
 
 def plotResults(df):
     sns.color_palette("Set2")
 
-    df['Loss'] = np.log(df['Loss'].astype(float))
+    #df['Loss'] = np.log(df['Loss'].astype(float))
 
-    title = f"Loss per Epoch with"
+    title = f"NN vs Kernel Regression"
     plot = sns.lineplot(
         data=df, x="N", y="Loss", hue="ModelType", ci=None
     ).set_title(title)
-    plot.figure.savefig(f"{title}_{d}_{a}.png")
+    plot.figure.savefig(f"{title}.png")
     plot.figure.clf()
 
-historyDf = pd.DataFrame(columns=[
-            "N", "Loss", "ModelType"
-])
-
-def addHistoryRow(df, loss, modelType):
+def addHistoryRow(df, n, loss, modelType):
+    print(df.head())
     new_row = pd.DataFrame(columns=df.columns)
-    new_row.loc[0] = [loss, modelType]
+    new_row.loc[0] = [n, loss, modelType]
     df = pd.concat([df, new_row], ignore_index=True)
     return df
 
-for n in ns:
-    trainXs, trainYs = genData(n, m)
-    testXs, testYs = genData(1000, m)
+############ BEGIN APPLICATION ############ 
 
-    net = Net(depth=m, width=d)
-    _ = net.fit(trainXs, trainYs)
-    nnError = calcError(net(testXs, testYs))
-    historyDf = addHistoryRow(historyDf, nnError, "Neureal Network")
+ns = [20, 40, 80, 160]
+d = 10
+m = 5
+lr = 0.1
+
+historyDf = pd.DataFrame(columns=[
+    "N", "Loss", "ModelType"
+])
+
+for n in ns:
+    trainXs, trainYs = genData(n, m, d)
+    testXs, testYs = genData(100, m, d)
+
+    #pp(trainXs)
+    #pp(trainYs)
+    net = Net(depth=m, width=d, initVariance=1/m)
+    lossPerEpoch = net.fit(torch.Tensor(trainXs), torch.Tensor(trainYs), lr)
+    pp(f"Loss per eopch: {lossPerEpoch}")
+    nnError = calcError(net(torch.Tensor(testXs)).detach().numpy().flatten(), testYs)
+
+    historyDf = addHistoryRow(historyDf, n, nnError, "Neureal Network")
 
     krr = KernelRidge(kernel=arcKernel)
+    print(trainXs)
     krr.fit(trainXs, trainYs)
-    krrError = calcError(krr.predict(testXs))
-    historyDf = addHistoryRow(historyDf, krrError, "Kernel Regression")
+    krrError = calcError(krr.predict(testXs), testYs)
+    historyDf = addHistoryRow(historyDf, n, krrError, "Kernel Regression")
 
 
+plotResults(historyDf)
